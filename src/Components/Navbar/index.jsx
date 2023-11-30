2039c1838bd207b70a4b8b235759aca5216f4efb
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-11-30 17:27:38 -0500

    28 Filtrando categorías con JavaScript

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-import { ShoppingCartIcon } from '@heroicons/react/24/solid'
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
@@ -18,6 +18,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/'
+            onClick={() => context.setSearchByCategory()}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -27,6 +28,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -36,6 +38,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -45,6 +48,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -54,6 +58,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -63,6 +68,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/others'
+            onClick={() => context.setSearchByCategory('others')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -102,8 +108,8 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li className='flex items-center'>
-          <ShoppingCartIcon className='h-6 w-6 text-black'></ShoppingCartIcon>
-          <div>{context.count}</div>
+          <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
+          <div>{context.cartProducts.length}</div>
         </li>
       </ul>
     </nav>
