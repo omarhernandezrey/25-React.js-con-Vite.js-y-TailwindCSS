950f26bcda56f2d0f4deda5748d5c54bcdebbc62
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-11-30 11:04:01 -0500

    25 Reto: órdenes de compra con TailwindCSS add icons

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,19 +1,26 @@
 import { ChevronRightIcon } from '@heroicons/react/24/solid'
-
+import { CalendarDaysIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
 
   return (
     <div className="flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80">
       <div className='flex justify-between w-full'>
-        <p className='flex flex-col'>
-          <span className='font-light'>01.02.23</span>
-          <span className='font-light'>{totalProducts} articles</span>
-        </p>
-        <p className='flex items-center gap-2'>
+        <div className='flex flex-col'>
+          <div className='flex items-center gap-2 mb-1'>
+            <CalendarDaysIcon className='h-6 w-6 text-black cursor-pointer' />
+            <span className='font-light'>30/11/2023</span>
+          </div>
+          <div className='flex items-center gap-2 mb-1'>
+            <ShoppingCartIcon className='h-6 w-6 text-black cursor-pointer' />
+            <span className='font-light'>{totalProducts} articles</span>
+          </div>
+        </div>
+        <div className='flex items-center mb-2 justify-end'>
           <span className='font-medium text-2xl'>${totalPrice}</span>
-          <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer' />
-        </p>
+          <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer ml-2' />
+        </div>
       </div>
     </div>
   )
